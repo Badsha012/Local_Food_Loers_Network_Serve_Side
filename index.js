f8539a9cc47492c27b129ca8332ce20298dde0b7
Badsha012 <badshasheikh361@gmail.com>
2025-11-11 08:38:10 +0600

    added ccors

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const cors=require('cors')
 const app = express()
 const port = 3000
 
+
+app.use(cors())
+
 app.get('/', (req, res) => {
-  res.send('server is Running !')
+  res.send('Server is Running Found !')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server is listening on port ${port}`)
 })
